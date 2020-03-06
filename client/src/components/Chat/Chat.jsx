75cb31032f564a01815c52bd44b489e54695c1ca
dc_strong <dc_strong@mail.ru>
2020-03-06 19:18:39 +0300

    message component

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,16 +1,18 @@
 import React, {Component} from "react";
 import io from 'socket.io-client';
+import Message from './Message';
 
 let socket;
-let ENDPOIND = "localhost:5000";
+// let ENDPOIND = "localhost:5000";
 export default class Chat extends Component {
 
-  socket = io(ENDPOIND);
+  // socket = io(ENDPOIND);
 
   render () {
     return (
       <div>
         <h2>Chat form</h2>
+        <Message/>
       </div>
     )
   }
